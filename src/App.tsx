88e131f5b04ca5e3ec7e31f1164abb31f2dcc72b
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-10-13 16:04:09 +0330

    Transpile JSX to ES2015 and show the result.
    * Import esbuild-wasm to the App component.
    * Create the startService function and run it when the App component
    mounts.
    * Save the result of the startService function to a ref and use it when
    a user clicks on the submit button. This will use the transform method
    of the object (type = esbuild.Service) to transpile JSX to JavaScript
    code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { ChangeEvent, useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 function App() {
+  const ref = useRef<esbuild.Service | null>(null);
   const [input, setInput] = useState("");
-  // code is the output of transpiling and bundling
   const [code, setCode] = useState("");
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "./esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setInput(e.target.value);
   };
 
-  const onButtonClick = () => {
-    console.log(input);
+  const onButtonClick = async () => {
+    if (!ref.current) return;
+
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(result.code);
   };
 
   return (
