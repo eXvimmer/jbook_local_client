f73c24a7eeafb60cbd4cb4f72551d920d8ee6fac
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-10-13 15:32:35 +0330

    Prepare the frontend for transpiling and bundling the code in browser.
    * Install esbuild: 'npm i --save-exact esbuild-wasm@0.8.27' for
    transpiling and bundling our code ((kinda) an alternative for webpack +
    babel).
    * Modify the App component to show some textarea, a button and a pre
    element (for the transpiled code).
    * Copy the esbuild.wasm file from node_modules/esbuild-wasm/esbuild.wasm to ./pubilc folder. We want this web assembly code to be fetched inside of the browser to do the transpiling and bundling (in the browser) for us.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { ChangeEvent, useState } from "react";
+
 function App() {
+  const [input, setInput] = useState("");
+  // code is the output of transpiling and bundling
+  const [code, setCode] = useState("");
+
+  const onTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
+  };
+
+  const onButtonClick = () => {
+    console.log(input);
+  };
+
   return (
     <div>
-      <h1>React App</h1>
+      <textarea value={input} onChange={onTextAreaChange}></textarea>
+      <div>
+        <button onClick={onButtonClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 }
