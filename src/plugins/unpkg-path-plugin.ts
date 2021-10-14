c997144c1bfd1a5ee2b5c5ffa96a58e482f6ff83
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-10-14 14:54:06 +0330

    Break up the resolve logic with filters.

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -12,21 +12,25 @@ export const unpkgPathPlugin: (inputCode: string) => esbuild.Plugin = (
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
+      // Handle root entry file of index.js
+      build.onResolve({ filter: /^index\.js$/ }, () => {
+        return {
+          path: "index.js",
+          namespace: "a",
+        };
+      });
+
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: esbuild.OnResolveArgs) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
+        };
+      });
+
+      // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: esbuild.OnResolveArgs) => {
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
-
-        if (args.path.includes("./") || args.path.includes("../")) {
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              "https://unpkg.com" + args.resolveDir + "/"
-            ).href,
-          };
-        }
-
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
