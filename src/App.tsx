dc4992352c7434aa4a14fa81ef8753c306226af0
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-10-16 15:02:00 +0330

    Refactor bundling logic
    * Create src/bundler/index.ts and move all the logic related to esbuild
    bundling from app to there.
    * Move plugins to bundler directory.

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,25 @@
-import * as esbuild from "esbuild-wasm";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
+import bundle from "./bundler";
 import CodeEditor from "./components/CodeEditor";
 import Preview from "./components/Preview";
-import { fetchPlugin } from "./plugins/fetch-plugin";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 function App() {
-  const ref = useRef<esbuild.Service | null>(null);
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://www.unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
+  /*
   useEffect(() => {
-    startService();
-
-    // cleanup
-    return () => {
-      ref.current && ref.current.stop();
-    };
+    // move to top
+    import { srevice } from "./bundler";
+    // stop the service when componet unmounts
+    return () => service.stop();
   }, []);
+  */
 
   const onButtonClick = async () => {
-    if (!ref.current) return;
+    const output = await bundle(input);
 
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    // result.outputFiles ? setCode(result.outputFiles[0].text) : setCode("");
-    result.outputFiles && setCode(result.outputFiles[0].text);
+    setCode(output);
   };
 
   return (
