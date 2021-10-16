8ca353194ebe04027c2be0bfb6d56d59a6518a78
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-10-16 03:34:36 +0330

    Configure the CodeEditor.
    * Add some options as props to the MonacoEditor component to render it
    differently.

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -2,7 +2,25 @@ import { FC } from "react";
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor: FC = () => {
-  return <MonacoEditor height="500px" theme="dark" />;
+  return (
+    <MonacoEditor
+      language="javascript"
+      height="500px"
+      theme="dark"
+      options={{
+        minimap: {
+          enabled: false,
+        },
+        wordWrap: "on",
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
