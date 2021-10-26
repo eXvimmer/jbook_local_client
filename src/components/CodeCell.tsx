5902bfade5a163213109a40b88a29589c4425d8e
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2021-10-26 18:53:03 +0330

    Add eager bundling.
    * Create an initial bundle if there's no bundle in CodeCell.
    * Turn off the dependency check in CodeCell.
    * Check for existence of bundle and bundle.loading in CodeCell.
      Then load the Preview component or a loading  bar (a progress element).
    * Create CodeCell.css and add some styling for thte progress bar
     ( most of the styling for the progress bar comes from bulma or bulmaswatch).

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -1,3 +1,5 @@
+import "./CodeCell.css";
+
 import { useEffect } from "react";
 import { useActions } from "../hooks/useActions";
 import { useTypedSelector } from "../hooks/useTypedSelector";
@@ -15,11 +17,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 1000);
 
     return () => clearTimeout(timer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle]);
 
   return (
@@ -37,7 +45,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} err={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
